0d3bd5e32682a6d8085940397221f5a167b7719e
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-03-29 16:49:07 -0400

    Finished database

--- seed.py
+++ seed.py
@@ -8,9 +8,19 @@ Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+session.query(Pokemon).delete()
+session.query(Move).delete()
+session.query(Stat).delete()
+
 charizard = Pokemon(name='Charizard', type='Fire')
 charizard.moves = [Move(name='Flamethrower'), Move(name='Fly'), Move(name='Blast Burn'), Move(name='Fire Punch')]
 charizard.stats = [Stat(name='ATTACK', value=12), Stat(name='DEFENSE', value=8)]
 
+new_pokemon = Pokemon(name='Pikachu', type='Electric')
+new_pokemon.moves = [Move(name='Thunderbolt'), Move(name='Quick Attack'), Move(name='Iron Tail'), Move(name='Volt Tackle')]
+new_pokemon.stats = [Stat(name='ATTACK', value=6), Stat(name='DEFENSE', value=5)]
+
+
+session.add(new_pokemon)
 session.add(charizard)
 session.commit()
