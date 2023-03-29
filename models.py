5fc8973395112391125c2581deda07875994302d
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-03-29 10:27:38 -0400

    two tables

--- models.py
+++ models.py
@@ -7,4 +7,23 @@ class Pokemon(Base):
     __tablename__ = "Pokemon"
 
     id = Column("key", Integer, primary_key = True)
-    category = Column("category", String)
+    pokemon = Column("Pokemon", String)
+
+    def __init__(self, pokemon):
+        self.pokemon = pokemon
+
+    def __repr__(self):
+        return f"{self.id} {self.pokemon}"
+    
+
+class Moveset(Base):
+    __tablename__ = "MoveSets"
+
+    id = Column("key", Integer, primary_key = True)
+    moveset = Column("MoveSet", String)
+
+    def __init__(self, moveset):
+        self.moveset = moveset
+
+    def __repr__(self):
+        return f"{self.id} {self.moveset}"
